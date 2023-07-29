7f4b5fa01d39f97e85face1c853791da13cdb387
elsa <jlocke8@gmail.com>
2023-07-28 21:44:13 -0700

    added automatic uinput device definitions from imported config file by placing all config dictionary items into a list and using the list a device definition

--- spacePanelControl/config.py
+++ spacePanelControl/config.py
@@ -162,8 +162,7 @@ button_uinput_map = {
                         }
 
 
-//Key to sound maps
-
+#Key to sound maps
 
 IC0A_sound_map = {     
 			0: "startupsequence.wav",
@@ -176,7 +175,148 @@ IC0A_sound_map = {
                         7: "startupsequence.wav",
                         }  
 
+IC0B_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
 
+IC1A_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
+
+IC1B_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
+
+IC2A_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
+
+IC2B_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
+
+IC3A_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
+
+IC3B_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
+
+IC4A_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
+
+IC4B_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
+
+IC5A_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
+
+IC5B_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
+
+IC6A_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
+
+IC6B_sound_map = {     
+			0: "startupsequence.wav",
+                        1: "startupsequence.wav",
+                        2: "startupsequence.wav",
+                        3: "startupsequence.wav",
+                        4: "startupsequence.wav",
+                        5: "startupsequence.wav",
+                        6: "startupsequence.wav",
+                        7: "startupsequence.wav",
+                        }  
 
 #create dictionary to hold sound mappings
 button_sound_map = {   
@@ -195,3 +335,105 @@ button_sound_map = {
                         '6A':IC6A_sound_map,
                         '6B':IC6B_sound_map,
                         }
+
+
+
+
+
+
+
+
+
+
+#enumeration definitions for each panel and button name in the spaceship
+class TopPanel_IC0_A(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2 
+class TopPanel_IC0_B(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2 
+
+class TopPanel_IC1_A(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2 
+class TopPanel_IC1_B(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2 
+
+class TopPanel_IC2_A(enum.Enum):
+    A0_IC2_Launch2=0 
+    A1_IC2_Toes=1
+    A2_IC2_Atootie=2
+    A3_IC2_ButtCrack=3
+    A4_IC2_DriedPee=4
+    A5_IC2_Poop=5
+    A6_IC2_FootFace=6
+    A7_IC2_Dabadu=7
+class TopPanel_IC2_B(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2     
+
+class FrontSidePanel_IC3_A(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2   
+class FrontSidePanel_IC3_B(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2   
+
+class FrontSidePanel_IC4_A(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2   
+class FrontSidePanel_IC4_B(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2   
+
+
+class BackSidePanel_IC5_A(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2   
+class BackSidePanel_IC5_B(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2   
+
+class BackSidePanel_IC6_A(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2   
+class BackSidePanel_IC6_B(enum.Enum):
+    A0_IC5_LifeSupport=1 
+    A1_IC5_PowerBank=2   
+
+
+#create dictionary to hold IO expander button definitions,  this is how the buttons are physically configured in the spaceship
+button_definitions = {  '0A':TopPanel_IC0_A,
+                        'TOPPANEL_IC0_PORTA':TopPanel_IC0_A,
+                        '0B':TopPanel_IC0_B,
+                        'TOPPANEL_IC0_PORTB':TopPanel_IC0_B,
+                        '1A':TopPanel_IC1_A,
+                        'TOPPANEL_IC1_PORTA':TopPanel_IC1_A,
+                        '1B':TopPanel_IC1_B,
+                        'TOPPANEL_IC1_PORTB':TopPanel_IC1_B,
+                        '2A':TopPanel_IC2_A,
+                        'TOPPANEL_IC2_PORTA':TopPanel_IC2_A,
+                        '2B':TopPanel_IC2_B,
+                        'TOPPANEL_IC2_PORTB':TopPanel_IC2_B,
+
+                        '3A':FrontSidePanel_IC3_A,
+                        'FRONTSIDEPANEL_IC3_PORTA':FrontSidePanel_IC3_A,
+                        '3B':FrontSidePanel_IC3_B,
+                        'FRONTSIDEPANEL_IC3_PORTB':FrontSidePanel_IC3_B,
+                        '4A':FrontSidePanel_IC4_A,
+                        'FRONTSIDEPANEL_IC4_PORTA':FrontSidePanel_IC4_A,
+                        '4B':FrontSidePanel_IC4_B,
+                        'FRONTSIDEPANEL_IC4_PORTB':FrontSidePanel_IC4_B,
+
+                        '5A':BackSidePanel_IC5_A,
+                        'BACKSIDEPANEL_IC5_PORTA':BackSidePanel_IC5_A,
+                        '5B':BackSidePanel_IC5_B,
+                        'BACKSIDEPANEL_IC5_PORTB':BackSidePanel_IC5_B,
+                        '6A':BackSidePanel_IC6_A,
+                        'BACKSIDEPANEL_IC6_PORTA':BackSidePanel_IC6_A,
+                        '6B':BackSidePanel_IC6_B,
+                        'BACKSIDEPANEL_IC6_PORTB':BackSidePanel_IC6_B
+                        }
+
+                      
--- spacePanelControl/spacePanelControl.py
+++ spacePanelControl/spacePanelControl.py
@@ -8,6 +8,8 @@ import time         #used to control delays
 import subprocess   #used to make system command line calls
 import enum         #used to make enumerations 
 
+import config       #external python file which holds configuration dictionaries specific to this spaceship program (button mapping to sounds and keypresses, etc)
+
 #setup variable to hold switch status data
 status_top_panel = list()
 status_backside_panel = list()
@@ -15,105 +17,21 @@ status_frontside_panel = list()
 button_status = [[hex(0x00) for x in range(8)] for y in range(2)]   #initialize 8x2 two dimensional array
 execute_list = list()
 
-#enumeration definitions for each panel and button name in the spaceship
-class TopPanel_IC0_A(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2 
-class TopPanel_IC0_B(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2 
 
-class TopPanel_IC1_A(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2 
-class TopPanel_IC1_B(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2 
+#create a list of all the uinput key definitions created from the config library to be defined as a uinput device
+uinputlist = []
+for key in config.button_uinput_map:    #get each top level key in dictionary
+    for subkey in config.button_uinput_map[key]:    #for each top level key, enter toplevel key and get every subkey
+        uinputlist.append(config.button_uinput_map[key][subkey])  #get each item in nested dictionary
+print(uinputlist)
 
-class TopPanel_IC2_A(enum.Enum):
-    A0_IC2_Launch2=0 
-    A1_IC2_Toes=1
-    A2_IC2_Atootie=2
-    A3_IC2_ButtCrack=3
-    A4_IC2_DriedPee=4
-    A5_IC2_Poop=5
-    A6_IC2_FootFace=6
-    A7_IC2_Dabadu=7
-class TopPanel_IC2_B(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2     
-
-class FrontSidePanel_IC3_A(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2   
-class FrontSidePanel_IC3_B(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2   
-
-class FrontSidePanel_IC4_A(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2   
-class FrontSidePanel_IC4_B(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2   
-
-
-class BackSidePanel_IC5_A(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2   
-class BackSidePanel_IC5_B(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2   
-
-class BackSidePanel_IC6_A(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2   
-class BackSidePanel_IC6_B(enum.Enum):
-    A0_IC5_LifeSupport=1 
-    A1_IC5_PowerBank=2   
-
-
-#create dictionary to hold IO expander button definitions,  this is how the buttons are physically configured in the spaceship
-button_definitions = {  '0A':TopPanel_IC0_A,
-                        'TOPPANEL_IC0_PORTA':TopPanel_IC0_A,
-                        '0B':TopPanel_IC0_B,
-                        'TOPPANEL_IC0_PORTB':TopPanel_IC0_B,
-                        '1A':TopPanel_IC1_A,
-                        'TOPPANEL_IC1_PORTA':TopPanel_IC1_A,
-                        '1B':TopPanel_IC1_B,
-                        'TOPPANEL_IC1_PORTB':TopPanel_IC1_B,
-                        '2A':TopPanel_IC2_A,
-                        'TOPPANEL_IC2_PORTA':TopPanel_IC2_A,
-                        '2B':TopPanel_IC2_B,
-                        'TOPPANEL_IC2_PORTB':TopPanel_IC2_B,
-
-                        '3A':FrontSidePanel_IC3_A,
-                        'FRONTSIDEPANEL_IC3_PORTA':FrontSidePanel_IC3_A,
-                        '3B':FrontSidePanel_IC3_B,
-                        'FRONTSIDEPANEL_IC3_PORTB':FrontSidePanel_IC3_B,
-                        '4A':FrontSidePanel_IC4_A,
-                        'FRONTSIDEPANEL_IC4_PORTA':FrontSidePanel_IC4_A,
-                        '4B':FrontSidePanel_IC4_B,
-                        'FRONTSIDEPANEL_IC4_PORTB':FrontSidePanel_IC4_B,
-
-                        '5A':BackSidePanel_IC5_A,
-                        'BACKSIDEPANEL_IC5_PORTA':BackSidePanel_IC5_A,
-                        '5B':BackSidePanel_IC5_B,
-                        'BACKSIDEPANEL_IC5_PORTB':BackSidePanel_IC5_B,
-                        '6A':BackSidePanel_IC6_A,
-                        'BACKSIDEPANEL_IC6_PORTA':BackSidePanel_IC6_A,
-                        '6B':BackSidePanel_IC6_B,
-                        'BACKSIDEPANEL_IC6_PORTB':BackSidePanel_IC6_B
-                        }
-
-                      
-
-#configure uinput virtual keyboard
-device = uinput.Device([
-        uinput.KEY_A,
-        uinput.KEY_B,
-        uinput.KEY_C,
-        ])
+#configure uinput virtual keyboard using the uinput key definitions
+device = uinput.Device(
+        uinputlist
+#        uinput.KEY_A,
+#        uinput.KEY_B,
+#        uinput.KEY_C,            
+        )
 
 device_mouse = uinput.Device([
                 uinput.BTN_LEFT,
@@ -149,9 +67,9 @@ def main():
     pygame.mixer.init(44100, -16,2,1024)
 
     #subprocess.run("espeak -v en-n 'start up sequence initialized...' -p 0 -a 200 -s 150", shell=True)
-    pygame.mixer.Sound("startupsequence.wav").play()
+    pygame.mixer.Sound("./sounds/startupsequence.wav").play()
 
-    print('\nbutton definitions dictionary:\n' + str(button_definitions) + '\n')
+    print('\nbutton definitions dictionary:\n' + str(config.button_definitions) + '\n')
 
     #configure and open correct serial connection
     foundPort = scanConnections()
@@ -208,7 +126,7 @@ def scanConnections():
             if testRead == "(^_^)\n":
                 connection = testConnect
                 #subprocess.run("espeak -v zh-yue 'serial connection established...' -p 0 -a 200 -s 150", shell=True)
-                pygame.mixer.Sound("serialconnectionest.wav").play()
+                pygame.mixer.Sound("./sounds/serialconnectionest.wav").play()
                 break        
             if (timeOutCount - timeOutStart) >= 5:
                 timeOut = True            
@@ -280,7 +198,8 @@ def executeChanges():
 
                         if portLetter == 'A':
                             if portStatus & (1<<0):
-                                device.emit_click(uinput.KEY_A)                              
+                                print(config.button_uinput_map['2A'][0])
+                                device.emit_click(config.button_uinput_map['2A'][0])                              
                             if portStatus & (1<<1):    
                                 device.emit_click(uinput.KEY_B)  
                             if portStatus & (1<<2):
